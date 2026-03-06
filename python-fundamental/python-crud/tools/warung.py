155a8e22c43d92ad7f254b23391a31f5b0ec4602
fathir <fathirsyahputra559@gmail.com>
2026-03-06 07:47:12 +0700

    feat: add warung mini module for inventory management

---
+++ python-fundamental/python-crud/tools/warung.py
@@ -0,0 +1,37 @@
+import main
+from services import db
+
+def add():
+    kode_barang = input('kode barang: ')
+    nama_barang = input('nama barang: ')
+    harga_barang = int(input('harga barang: '))
+    stok_barang = int(input('stok barang: '))
+    
+    save = db.insert_item(kode_barang, nama_barang, harga_barang, stok_barang)
+    
+def cek_barang():
+    items = db.fetch_item()
+    for item in items:
+        kode_barang = item [1]
+        nama_barang = item [2]
+        harga_barang = item [3]
+        stok_barang = item [4]
+        print(f'''
+kode barang: {kode_barang}
+{nama_barang}| Rp {harga_barang}
+stok barang: {stok_barang}
+''')
+    
+def start():
+    while True:
+         menu = int(input('menu:\n1.Tambah Barang\n2.Cek Barang\n3.Kembali\n\nsilahkan pilih: '))
+         if menu == 1:
+             add()
+         elif menu == 2:
+             cek_barang()
+         elif menu == 3:
+              main.menu()
+         print('ini warung')
+    
+if __name__ == '__main__':
+    start()
