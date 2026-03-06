8a639e70f81d9ee2b18da3f8767b0bab3eeda9d5
fathir <fathirsyahputra559@gmail.com>
2026-03-06 07:47:22 +0700

    feat: add database interaction module

---
+++ python-fundamental/python-crud/services/db.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="market_mini"
+)
+
+def insert_item(kode_barang, nama_barang, harga_barang, stok_barang):
+    cursor = db.cursor() 
+    cursor.execute("INSERT INTO penyimpanan (kode_barang, nama_barang, harga_barang, stok_barang) VALUES (%s, %s, %s, %s)", (kode_barang, nama_barang, harga_barang, stok_barang))
+    db.commit()
+    if cursor.rowcount > 0:
+        print('\ndata berhasil dibuat\n')
+    else:
+        print('\ndata gagal di buat\n')
+        
+def fetch_item():
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM penyimpanan")
+    return cursor.fetchall()
+    
+    
